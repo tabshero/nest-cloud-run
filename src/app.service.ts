da6003504d74e2fc0a7bbfba68be51c79aca6f41
Akhil <akhil@eapl.us>
2023-08-08 08:01:45 +0530

    updated app file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Akhil !!';
+    return 'This is a nest project !!';
   }
 }
